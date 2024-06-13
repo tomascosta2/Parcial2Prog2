7de4f0ebc3c2bc3da5ca8f58af279ccb409fb7e1
tomas <tomascostapp@gmail.com>
2024-06-13 16:02:05 -0300

    Clases a persistencia

--- target/generated-sources/annotations/ldc1/tpprogramacionjpa/logica/Persona_.java
+++ target/generated-sources/annotations/ldc1/tpprogramacionjpa/logica/Persona_.java
@@ -4,7 +4,7 @@ import jakarta.persistence.metamodel.SingularAttribute;
 import jakarta.persistence.metamodel.StaticMetamodel;
 import javax.annotation.processing.Generated;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-13T11:51:30", comments="EclipseLink-3.0.2.v20210716-re8d4b571c9")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-13T15:57:24", comments="EclipseLink-3.0.2.v20210716-re8d4b571c9")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
